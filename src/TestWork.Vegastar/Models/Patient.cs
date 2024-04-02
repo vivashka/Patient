f180af897646410ad3eb0fdde85598521938c281
Vladimir <vovac8908@gmail.com>
2024-04-02 11:02:52 +0500

    Class (feat): #1 add class Patient

---
+++ src/TestWork.Vegastar/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace TestWork.Vegastar.Models
+{
+    public class Patient
+    {
+        public int Name { get; set; }
+        public int SurName { get; set; }
+        public int Patronymic { get; set; }
+        public int Age { get; set; }
+        public int Snils { get; set; }
+
+        public List<Patient> Desserelize<Patient>(string json)
+        {
+
+            
+        }
+    }
+}
